a93f3ac6d91612f48296f51d362652134d3a22e9
Louis Goessling <louis@goessling.com>
2014-02-18 17:29:58 -0600

    Added ovveride CS

--- Team2129-2014/src/team2129/frc2014/OverrideControlSystem.java
+++ Team2129-2014/src/team2129/frc2014/OverrideControlSystem.java
@@ -14,7 +14,24 @@ public class OverrideControlSystem {
         this.robot=robot;
     }
     
-    public void update(){
-        
+    public void update(){ //6/7 8/9 10/11
+        if (this.robot.getButtonPressed( 6)){
+            this.robot.pullCS.pullbackMotor.set(-1);
+        }
+        if (this.robot.getButtonPressed( 7)){
+            this.robot.pullCS.pullbackMotor.set( 1);
+        }
+        if (this.robot.getButtonPressed( 8)){
+            this.robot.fireCS.releaseMotor .set(-1);
+        }
+        if (this.robot.getButtonPressed( 9)){
+            this.robot.fireCS.releaseMotor .set( 1);
+        }
+        if (this.robot.getButtonPressed(10)){
+            this.robot.armCS.armMotor      .set(-1);
+        }
+        if (this.robot.getButtonPressed(11)){
+            this.robot.armCS.armMotor      .set( 1);
+        }
     }
 }
